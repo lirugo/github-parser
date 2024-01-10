247857a950cd17dd6d43a7a420d34a2660bbf218
Rostyslav Shynlov <rshynlov@gen2dev.com>
2024-01-10 17:06:27 +0200

    [no-jira] Implemented 2 words counter methods

--- src/main/java/com/lirugo/github/parser/service/ParserServiceImpl.java
+++ src/main/java/com/lirugo/github/parser/service/ParserServiceImpl.java
@@ -2,10 +2,13 @@ package com.lirugo.github.parser.service;
 
 import com.lirugo.github.parser.model.RepoFile;
 import com.lirugo.github.parser.model.Word;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -29,12 +32,8 @@ public class ParserServiceImpl implements ParserService {
         continue;
       }
 
-      var content = file.getContent().get();
-      // TODO implement my own algorithm
-      // TODO
-      var words = content
-          .replaceAll("[^a-zA-Z ]", "")
-          .split("\\s+");
+      var words = splitTextToWords(file.getContent());
+
       for (var word : words) {
         if (word.length() <= letterLimit && !word.isBlank()) {
           frequencyMap.merge(word, 1, Integer::sum);
@@ -45,7 +44,52 @@ public class ParserServiceImpl implements ParserService {
     return frequencyMap.entrySet().stream()
         .map(entry -> new Word(entry.getKey(), entry.getValue()))
         .sorted(Comparator.comparingInt(Word::frequency).reversed())
+        .filter(word -> word.word().length() <= letterLimit)
         .limit(topLimit)
         .toList();
+  }
+
+  private List<String> splitTextToWords(Optional<String> content) {
+    return content
+        .map(this::getWords)
+//        .map(this::splitWords)
+        .orElse(List.of());
+  }
+
+  /**
+   * Here i collect all words from text. I use StringBuilder to collect letters from word. When i
+   * meet non-letter symbol i add word to list and clear StringBuilder. I use pointer to iterate
+   * through text.
+   *
+   * @param content is just a text from file
+   * @return list of words
+   */
+  private List<String> getWords(String content) {
+    List<String> words = new ArrayList<>();
+    var sb = new StringBuilder();
+
+    for (var index = 0; index < content.length(); index++) {
+      while (index < content.length() && Character.isLetter(content.charAt(index))) {
+        sb.append(content.charAt(index++));
+      }
+
+      if (!sb.isEmpty()) {
+        words.add(sb.toString().toLowerCase());
+        sb = new StringBuilder();
+      }
+
+    }
+
+    return words;
+  }
+
+  /**
+   * Regex split is slower than my method TODO check it
+   *
+   * @param content
+   * @return
+   */
+  private List<String> splitWords(String content) {
+    return Arrays.asList(content.toLowerCase().split("\\W+"));
   }
 }
--- src/test/java/com/lirugo/github/parser/service/ParserServiceImplTest.java
+++ src/test/java/com/lirugo/github/parser/service/ParserServiceImplTest.java
@@ -1,24 +1,25 @@
 package com.lirugo.github.parser.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.lirugo.github.parser.model.Repo;
 import com.lirugo.github.parser.model.RepoFile;
 import com.lirugo.github.parser.model.Word;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.List;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 
 @Slf4j
 class ParserServiceImplTest {
 
   @Test
   void testCountWordFrequency() {
-    File readme1File = new File(getClass().getClassLoader().getResource("data/README-1.md").getFile());
+    File readme1File = new File(
+        getClass().getClassLoader().getResource("data/README-1.md").getFile());
     var readme1Content = "";
     try {
       readme1Content = Files.readString(readme1File.toPath());
@@ -30,17 +31,15 @@ class ParserServiceImplTest {
 
     Repo repo = new Repo(1, "testOwner");
     RepoFile file1 = new RepoFile(repo, "name", "path1", 1, Optional.of("hello world hello"));
-
     RepoFile file2 = new RepoFile(repo, "name", "path2", 1, Optional.of(readme1Content));
     List<RepoFile> files = List.of(file1, file2);
 
-    List<Word> result = service.countWordFrequency(files, 5, 3);
+    List<Word> result = service.countWordFrequency(files, 10, 3);
 
     assertEquals(3, result.size());
-    assertEquals("to", result.get(0).word());
-    assertEquals(3, result.get(0).frequency());
-    assertEquals("hello", result.get(1).word());
-    assertEquals(2, result.get(1).frequency());
-
+    assertEquals("foobar", result.get(0).word());
+    assertEquals(8, result.get(0).frequency());
+    assertEquals("to", result.get(1).word());
+    assertEquals(4, result.get(1).frequency());
   }
 }
